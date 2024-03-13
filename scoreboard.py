1099a099a95dd082c12e15b80f402eab85f21ff4
chillcoffee <ruffaresentes@me.com>
2024-03-13 15:02:59 +0800

    Create high score file

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        self.highscore = self.read_highscore()
         self.color("white")
         self.hideturtle()
         self.penup()
@@ -27,8 +27,20 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            self.write_highscore()
         self.score = 0
         self.update_scoreboard()
+
+    def read_highscore(self):
+        with open("highscore.txt") as highscore_file:
+            highscore = highscore_file.read()
+        return int(highscore)
+
+    def write_highscore(self):
+        with open("highscore.txt", mode="w") as highscore_file:
+            highscore_file.write(str(self.highscore))
+
+
 
 
     # def game_over(self):
